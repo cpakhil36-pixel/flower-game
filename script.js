5ff851c94c10c5b432ab3f1c394244dc9a3a5da9
cpakhil36-pixel <cpakhil36@gmail.com>
2026-03-18 12:27:13 +0530

    Add flower generation functionality in script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 
+function start(){
+
+let flowers = [
+"blackrose.png",
+"lavander.webp",
+"meeronrose.webp",
+"sunflower.png",
+"whiterose.png"
+];
+
+for(let i=0;i<25;i++){
+
+let f = document.createElement("div");
+f.classList.add("flower");
+
+let img = document.createElement("img");
+
+img.src = flowers[Math.floor(Math.random()*flowers.length)];
+
+f.style.left = Math.random()*100 + "%";
+
+f.appendChild(img);
+
+document.body.appendChild(f);
+}
+
+}
