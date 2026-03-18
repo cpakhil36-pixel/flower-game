05c361a87902bffa1ea2adc007eb9d365bf8430b
cpakhil36-pixel <cpakhil36@gmail.com>
2026-03-18 11:46:20 +0530

    Remove flower tile generation from script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-let board = document.getElementById("game-board");
 
-let flowers = ["🌸","🌼","🌻","🌷","🌹"];
-
-for(let i=0;i<64;i++){
-
-let tile = document.createElement("div");
-tile.classList.add("tile");
-
-let randomFlower = flowers[Math.floor(Math.random()*flowers.length)];
-
-tile.innerHTML = randomFlower;
-
-board.appendChild(tile);
-
-}
