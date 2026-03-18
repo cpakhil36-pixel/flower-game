2cd5196aa836108807c3091d258f7588321620d8
cpakhil36-pixel <cpakhil36@gmail.com>
2026-03-18 12:38:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+function start(){
 
+let flowers = ["blackrose.png","whiterose.png","meeronrose.webp"];
+
+for(let i=0;i<40;i++){
+
+let f = document.createElement("div");
+f.classList.add("rose");
+
+let img = document.createElement("img");
+
+img.src = flowers[Math.floor(Math.random()*flowers.length)];
+
+f.style.left = Math.random()*100 + "%";
+
+f.appendChild(img);
+
+document.body.appendChild(f);
+}
+
+setTimeout(()=>{
+document.getElementById("message").style.opacity = 1;
+},2000);
+
+}
